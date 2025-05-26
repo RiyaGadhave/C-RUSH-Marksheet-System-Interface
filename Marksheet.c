e40100565a8defab43eb207b41fbbaa2b45aa38f
RiyaGadhave <riyagadhavecse@gmail.com>
2025-05-27 01:20:37 +0530

    Update Marksheet.c

--- Marksheet.c
+++ Marksheet.c
@@ -1,3 +1,5 @@
+CODE:
+
 #include <stdio.h>
 #include <string.h>
 
@@ -85,7 +87,56 @@ printf(" Enter Term/Sem =");
     printf("Result = %s\n", Result);
     printf("Teacher Signature\t");
     printf("Principal Signature\n");
-    printf("Date of Issue: 23 May 2025\n");
+    
 
     return 0;
 }
+
+
+
+
+OUTPUT:-
+Enter school/College name: GCOEY
+Enter ACADEMIC YEAR = 2025
+ Enter Term/Sem =1
+Enter student name= Sima Jadhav
+Enter student Class= 1st year
+Enter Stream/Branch of student= co.puter engg.
+Enter Mother name= manisha
+Enter student Seat no= 16
+Enter name of Subject 1= math
+Enter marks for math (out of 100)= 89
+Enter name of Subject 2= bcme
+Enter marks for bcme (out of 100)= 88
+Enter name of Subject 3= beee
+Enter marks for beee (out of 100)= 98
+Enter name of Subject 4= physics
+Enter marks for physics (out of 100)= 87
+Enter name of Subject 5= pps
+Enter marks for pps (out of 100)= 97
+
+    ------MARKSHEET-----
+
+School/College name:GCOEY
+        Academic year 2025
+            Term/Sem : 1
+STUDENT NAME: Sima Jadhav
+Seat no.: 16
+Mother Name: manisha
+Class: 1st year
+Stream/Branch: co.puter engg.
+
+Subject      Marks
+math         89
+bcme         88
+beee         98
+physics      87
+pps          97
+Total Marks = 459.00 / 500
+Percentage = 91.80%     Grade = A
+Result = Pass
+
+Teacher Signature       Principal Signature
+
+
+[Process completed - press Enter]
